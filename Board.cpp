cfc9b7d48f598c88bf36532c1c0e205c62157c65
cmurphy-06 <D00279403@student.dkit.ie>
2026-05-03 23:03:12 +0100

    Started file reading and splitting up line in board.cpp to push bugs gotten from file

--- Board.cpp
+++ Board.cpp
@@ -3,3 +3,54 @@
 //
 
 #include "Board.h"
+
+#include "Bug.h"
+#include <iostream>
+#include <sstream>
+
+#include "Crawler.h"
+#include "Hopper.h"
+using namespace std;
+
+#include <fstream>
+
+void Board::loadBugs() {
+    ifstream fin("bugs.txt");
+    if (fin) {
+        string line;
+        std::stringstream test(line);
+        std:string semicolon;
+        std::vector<std::string> bugList;
+        /*https://stackoverflow.com/questions/10058606/splitting-a-string-by-a-character */
+        while (std::getline(test, semicolon, ';')) {
+            bugList.push_back(semicolon);
+        }
+
+        /* https://www.codecademy.com/article/how-to-convert-a-string-to-an-integer-in-c */
+        int id = std::stoi(bugList[1]);
+        int x = std::stoi(bugList[2]);
+        int y = std::stoi(bugList[3]);
+        int direction = std::stoi(bugList[4]);
+        int health = std::stoi(bugList[5]);
+
+        string type = bugList[0];
+        if (type == "C") {
+            /* Crawler */
+            pair<int, int> position = {x,y};
+            char bugType = 'C';
+            bugs.push_back(new Crawler(id, position, direction, health));
+        }
+        else if (type == "H") {
+            pair<int, int> position = {x,y};
+            int hopLength = std::stoi(bugList[6]);
+            bugs.push_back(new Hopper(id, position, direction, health, hopLength));
+        }
+        else {
+            cout << "Error finding bug type" << endl;
+        }
+
+    }
+    else {
+        cout << "No file found" << endl;
+    }
+}
--- Board.h
+++ Board.h
@@ -4,16 +4,20 @@
 
 #ifndef A_BUGS_LIFE_BOARD_H
 #define A_BUGS_LIFE_BOARD_H
+#include <vector>
 
+#include "Bug.h"
 
 class Board {
 private:
     int width;
     int height;
+    std::vector<Bug*> bugs;
 public:
     Board(int width, int height);
     ~Board();
 
+    static void loadBugs();
     void displayAllBugs();
     void GetBugByID(int id);
     void displayHistoryOfAllBugs();
