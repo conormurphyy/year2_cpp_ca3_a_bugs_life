6f61552cb19c92a6d7f3d551b11f6a3cf1d34333
cmurphy-06 <D00279403@student.dkit.ie>
2026-05-07 20:06:00 +0100

    Fixed backtracker

--- bugs/Backtracker/Backtracker.cpp
+++ bugs/Backtracker/Backtracker.cpp
@@ -31,16 +31,16 @@ void Backtracker::move() {
     }
 
     if (direction == 1) {
-        position.second = position.second + 1;
-    }
-    else if (direction == 2) {
-        position.first = position.first - 1;
-    }
-    else if (direction == 3) {
         position.second = position.second - 1;
     }
-    else if (direction == 4) {
+    else if (direction == 2) {
         position.first = position.first + 1;
+    }
+    else if (direction == 3) {
+        position.second = position.second + 1;
+    }
+    else if (direction == 4) {
+        position.first = position.first - 1;
     }
     else {
         cout << "Error" << endl;
